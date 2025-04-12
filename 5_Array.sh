db889720af9949472b7a62edcbf4afe4c52f4a85
Onkar27vk <164345058+Onkar27vk@users.noreply.github.com>
2025-04-12 17:48:00 +0530

    Create 5_Array.sh

---
+++ 5_Array.sh
@@ -0,0 +1,12 @@
+myArray=(1 27 5 1.5 hellow "onkar chetana!")
+
+
+echo "${myArray[1]}"
+echo "${myArray[2]}"
+echo "${myArray[5]}"
+
+echo "all values are ${myArray[*]}"
+
+
+echo "${myArray[*]:2:3}"
+~
