28aa13c869a9816746d41aae4d90d757b73683ba
Onkar27vk <164345058+Onkar27vk@users.noreply.github.com>
2025-04-12 17:48:47 +0530

    Create 6_KeyVarArray.sh

---
+++ 6_KeyVarArray.sh
@@ -0,0 +1,11 @@
+#how to store the key values pairs
+
+
+declare -A myArray
+
+myArray=( [name]=prashant [age]=28 [city]=paris )
+
+
+echo "Name is ${myArray[name]}"
+echo "Age is ${myArray[age]}"
+~
