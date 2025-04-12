68fdbe6639eceba1bda4823c214972a540327c9b
Onkar27vk <164345058+Onkar27vk@users.noreply.github.com>
2025-04-12 17:46:38 +0530

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,26 @@
+#script to show how to use variable
+
+
+a=10
+name="onkar"
+age=25
+
+
+echo "my name is $name"
+echo "my age is $age and no is $a"
+
+
+name="chetana"
+echo "my name is $name"
+
+
+
+#var to store the command output
+
+
+HOSTNAME=$(hostname)
+echo "Name of the machine is $HOSTNAME"
+
+PATH=$(pwd)
+echo "path is $PATH"
+~
